57fe2399f9f91599bf36ae0092c4a5be0645635c
Calum Bruton <calumbruton@gmail.com>
2018-02-01 18:32:48 -0500

    First Commit

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	//Start is called on the first frame the script is active
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	//Fixed update is called just before rendering any physics calculations
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	//Update is called before rendering a fram, this is where our game code will go
+	void Update ()
+	{}
+}
+
